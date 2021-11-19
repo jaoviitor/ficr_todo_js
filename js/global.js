79cab53e08fcb610b45498f1a4efd3e70ebff61f
João Vitor Vieira da Silva <joaovitor.vieira@a.ficr.edu.br>
2021-11-18 21:48:18 -0300

    criando função para renderizar os cards na tela

--- js/global.js
+++ js/global.js
@@ -1,9 +1,9 @@
 const cardNovaLista = document.getElementById('cardNovaLista')
 const btnNovaLista = document.getElementById('btnNovaLista')
 const formNovaLista = document.getElementById('novaLista')
+const divListas = document.getElementById('listas')
 
-
-const listas = []
+let listas = []
 
 btnNovaLista.addEventListener('click', function(){
     cardNovaLista.style.display = 'block'
@@ -20,6 +20,7 @@ formNovaLista.addEventListener('submit', function(event) {
         inputTitulo.value = ''
         listas.push(novaLista)
         window.localStorage.listas = JSON.stringify(listas)
+        renderList()
     }
     console.log(listas)
 })
@@ -27,3 +28,28 @@ formNovaLista.addEventListener('submit', function(event) {
 formNovaLista.addEventListener('reset', function(event) {
     cardNovaLista.style.display = 'none'
 })
+// função para adicionar os itens na tela
+function renderList () {
+    listas = JSON.parse(window.localStorage.listas)
+    // limpando a div quer recebe os cards das listas
+    divListas.innerHTML = ""
+    // para cada lista arrey de listas, cria um novo card na tela
+    listas.map(lista => {
+        // criando um titulo pro card
+    let h2Tag = document.createElement('H2')
+    // adiciona o título da lista salca no título do card
+    h2Tag.innerHTML = lista.titulo
+    // cria uma div para ser o card da lista
+    let divTag = document.createElement('DIV')
+    // adiciona a classe card-lista a div para o css
+    divTag.setAttribute('class','card-lista')
+    // adiciona o h2 a div
+    divTag.appendChild(h2Tag)
+    // adiciona a duv.card-lista a div que contem todas as listas na tela
+    divListas.appendChild(divTag)
+    })
+}
+// verifica se existe algo no localStorage para iniciar
+if (window.localStorage.listas?.length > 0) {
+    renderList
+}
